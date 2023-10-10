114bfe7eea2d7c38e6da0c94094712bf971ac6c4
Loki99911 <nikita_kresik@i.ua>
2023-10-10 14:51:51 +0300

    Изменение сборки

--- src/components/CustomButton/CustomButton.jsx
+++
@@ -1,5 +0,0 @@
-import { StyledBtn } from "./CustomButton.styled";
-
-export const OrangeButton = ({ children, onClick }) => {
-  return <StyledBtn onClick={onClick}>{children}</StyledBtn>;
-};
--- src/pages/ExperiencePage/ExperiencePage.jsx
+++
@@ -1,3 +0,0 @@
-export const ExperiencePage = () => {
-  return <>ExperiencePage</>;
-};
